2ba9546469888bcfc3b471417c957dc052c3cbc5
Nitin <nsinghboss02@gmail.com>
2021-10-08 16:40:15 +0530

    Initial commit

---
+++ src/com/nitincodes/Main.java
@@ -0,0 +1,33 @@
+package com.nitincodes;
+
+public class Main {
+    // ceiling of a number in an array which is sorted in descending order:
+    public static void main(String[] args) {
+        int[] arr = {134,123,122,112,111,100};
+        int ans = ceilingNumber(arr,112);
+        System.out.println(ans);
+
+    }
+    static int ceilingNumber(int[] arr, int target){
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(start>arr[mid]){
+                end = mid-1;
+            }
+            else if(start<arr[mid]){
+                start = mid+1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return end;
+
+    }
+
+
+
+
+    }
